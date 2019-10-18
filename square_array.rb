f61c97c5c96980ad6fc48cb685140469d6282e47
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-18 04:51:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each do |number|
     new_array.push(number ** 2)
   end
   return new_array
+=======
+  array.collect do |number|
+    number ** 2
+  end
+>>>>>>> 817a68359b32f3c6acfca5eabf069fcdb682404d
 end
